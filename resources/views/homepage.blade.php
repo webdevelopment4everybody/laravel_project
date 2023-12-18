0217044dc508f134b465c5c982b585f947e25321
webdevelopment4everybody <migle.pupeikyte@mediapark.com>
2023-12-18 17:02:56 +0200

    style fix

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="mx-auto text-lg text-black">
+    <div class="mx-auto text-lg text-black container mt-[50px]">
         <p>Migle Pupeikyte, PIT-20-I-NT</p>
     </div>
 @endsection
